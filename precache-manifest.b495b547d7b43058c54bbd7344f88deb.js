a336589345e5d4b476f9927de2733edecc72973b
Nazrul Babu <nazrulbabu@Nazruls-MBP.fios-router.home>
2021-07-27 21:32:28 -0400

    Updates

---
+++ precache-manifest.b495b547d7b43058c54bbd7344f88deb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6070e7503a42b0925b500f398f93417d",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "d844cfcf834db4f96824",
+    "url": "/deploy/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "1d68df9ca8ddb0ba7d76",
+    "url": "/deploy/static/css/main.e4bb8ced.chunk.css"
+  },
+  {
+    "revision": "d844cfcf834db4f96824",
+    "url": "/deploy/static/js/2.f8d010aa.chunk.js"
+  },
+  {
+    "revision": "1d68df9ca8ddb0ba7d76",
+    "url": "/deploy/static/js/main.afcf8ba0.chunk.js"
+  },
+  {
+    "revision": "d495f61e9c7249f9c5bc",
+    "url": "/deploy/static/js/runtime-main.b7db6c38.js"
+  },
+  {
+    "revision": "d9487ca1ce831388674967b29bae1b80",
+    "url": "/deploy/static/media/bsweet.d9487ca1.png"
+  },
+  {
+    "revision": "6110131f36a1082d70a99528a4a5b105",
+    "url": "/deploy/static/media/car.6110131f.png"
+  },
+  {
+    "revision": "cafadadf92f4473ba0d577b3ad98ab5e",
+    "url": "/deploy/static/media/car2.cafadadf.png"
+  },
+  {
+    "revision": "6e968c9f6a27b655d585b49447299498",
+    "url": "/deploy/static/media/car3.6e968c9f.png"
+  },
+  {
+    "revision": "d0012ef1c346ff4cf07f9c4482f79ef6",
+    "url": "/deploy/static/media/car4.d0012ef1.png"
+  }
+]);
